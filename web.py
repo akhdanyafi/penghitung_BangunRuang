d7911d637ef57c0263dfbb3c7192ec30b024bb42
akhdanyafi <73579871+akhdanyafi@users.noreply.github.com>
2024-04-08 17:27:36 +0700

    Update web.py

--- web.py
+++ web.py
@@ -25,8 +25,19 @@ st.write("Segitiga")
 alas = st.number_input("Masukkan Alas Segitiga:",0)
 tinggi = st.number_input("Masukkan Tinggi Segitiga:",0)
 
-hitung_luas = st.button("hitung luas segitiga")
+hitung_luas_segitiga = st.button("hitung luas segitiga")
 
-if hitung_luas:
+if hitung_luas_segitiga:
     luas_sgitiga = (1/2) * alas * tinggi
     st.write("luas segitiga adalah:",luas_sgitiga)
+
+#persegi panjang
+st.write("Persegi Panjang")
+panjang = st.number_input("Masukkan panjang segitiga", min_value = 0, max_value = 999999999999)
+lebar = st.number_input("Masukkan lebar", min_value = 0, max_value = 99999999999)
+
+hitung_luas_PersegiPanjang = st.button("hitung luas")
+
+if hitung_luas_PersegiPanjang:
+    luas_persegiPanjang = panjang * lebar
+    st.success(f"Luas Persegi Panjang: {luas_persegiPanjang}")
